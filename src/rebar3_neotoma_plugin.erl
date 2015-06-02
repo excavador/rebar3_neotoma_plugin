a8fdab18a6e3cb660da82c27d25e3418db84a510
Oleg Tsarev <oleg@oleg.sh>
2015-06-02 11:01:50 +0300

    fix

--- src/rebar3_neotoma_plugin.erl
+++ src/rebar3_neotoma_plugin.erl
@@ -35,7 +35,7 @@ Provider = providers:create([
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
     rebar_api:info("Running neotoma...", []),
-    case rebar_state:get(State, escript_main_app, undefined) of
+    _Ignore = case rebar_state:get(State, escript_main_app, undefined) of
         undefined ->
             Dir = rebar_state:dir(State),
             case rebar_app_discover:find_app(Dir, all) of
@@ -48,11 +48,11 @@ do(State) ->
                         _ ->
                             run_neotoma(AppInfo, State)
                     end,
-                    {ok, State};
+                        {ok, State};
                 _ ->
                     ?PRV_ERROR(no_main_app)
             end;
-        Name ->
+        _Name ->
             ok
     end,
     {ok, State}.
@@ -61,59 +61,25 @@ run_neotoma(App, State) ->
     Source = rebar_app_info:source(App),
     Dir = rebar_state:dir(State),
     rebar_api:debug("source=~p dir=~p", [Source, Dir]),
-    rebar_base_compiler:run(State,
-                            [],
-                            Source,
-                            "peg",
-                            Source,
-                            "erl",
-                            fun(Source, Target, State) ->
-                                    compile_peg(State, Source, Target, [], Dir, Source)
+    rebar_base_compiler:run(State, [], Source, "peg", Source, "erl",
+                            fun(SourceF, TargetF, StateF) ->
+                                compile_peg(StateF, SourceF, TargetF, [], Dir, SourceF)
                             end,
-                            [{check_last_mod, false},
-                             {recursive, option(recursive, [])}]).
+                            [{check_last_mod, false}]).
 
-compile_peg(State, Source, Target, DtlOpts, Dir, OutDir) ->
-    case needs_compile(Source, Target, DtlOpts) of
+compile_peg(_State, Source, Target, _Options, _Dir, OutDir) ->
+    case needs_compile(Source, Target) of
         true ->
-            do_compile(State, Source, Target, DtlOpts, Dir, OutDir);
+            neotoma:file(Source, [{output, OutDir}]);
         false ->
             skipped
     end.
 
-do_compile(State, Source, Target, DtlOpts, Dir, OutDir) ->
-    eof.
-%    CompilerOptions = option(compiler_options, DtlOpts),
-%
-%    Sorted = proplists:unfold(
-%               lists:sort(
-%                 [{out_dir, OutDir},
-%                  {doc_root, filename:join(Dir, option(doc_root, DtlOpts))},
-%                  {custom_tags_dir, option(custom_tags_dir, DtlOpts)},
-%                  {compiler_options, CompilerOptions}])),
-%
-%    %% ensure that doc_root and out_dir are defined,
-%    %% using defaults if necessary
-%    Opts = lists:ukeymerge(1, DtlOpts, Sorted),
-%    rebar_api:debug("Compiling \"~s\" -> \"~s\" with options:~n    ~s",
-%                    [Source, Target, io_lib:format("~p", [Opts])]),
-%    case erlydtl:compile_file(ec_cnv:to_list(Source),
-%                              list_to_atom(module_name(Target)),
-%                              Opts) of
-%        {ok, _Mod} ->
-%            ok;
-%        {ok, _Mod, Ws} ->
-%            rebar_base_compiler:ok_tuple(State, Source, Ws);
-%        error ->
-%            rebar_base_compiler:error_tuple(State, Source, [], [], Opts);
-%        {error, Es, Ws} ->
-%            rebar_base_compiler:error_tuple(State, Source, Es, Ws, Opts)
-%    end.
+needs_compile(Source, Target) ->
+    LM = filelib:last_modified(Target),
+    LM < filelib:last_modified(Source).
 
-%% When an exception is raised or a value returned as
-%% `{error, {?MODULE, Reason}}` will see the `format_error(Reason)`
-%% function called for them, so a string can be formatted explaining
-%% the issue.
+
 -spec format_error(any()) -> iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
